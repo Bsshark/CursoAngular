dd11571c897749c1ccfd118d67385d373577140f
Abraham <abrahamlega@gmail.com>
2024-01-25 23:26:11 +0000

    104. Gif App Iniciada

---
+++ 03-gifs-app/src/app/gifs/components/card-list/card-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { Gif } from '../../interfaces/gifs.interfaces';
+
+@Component({
+  selector: 'gifs-card-list',
+  templateUrl: 'card-list.component.html'
+})
+
+export class CardListComponent {
+  constructor() { }
+
+  @Input()
+  public gifs: Gif[] = [];
+
+}
---
+++ 03-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,25 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
+
+@Component({
+  selector: 'gifs-search-box',
+  template: `
+  <h5>Buscar</h5>
+  <input type="text" class="form-control" placeholder="Buscar gif..." (keyup.enter)="searchTag()" #txtTagInput >
+  `
+})
+
+export class SearchBoxComponent {
+
+  @ViewChild('txtTagInput')
+  public tagInput!:ElementRef<HTMLInputElement>;
+
+  constructor(private gifsService: GifsService) { }
+
+  searchTag() {
+    const newTag = this.tagInput.nativeElement.value;
+    this.gifsService.searchTag(newTag);
+    this.tagInput.nativeElement.value = "";
+  }
+
+}
